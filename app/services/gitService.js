4ba0e4c0dedc9ad0eac80652c7e75b91c3eab316
Caio <caio.michel@gmail.com>
2017-10-20 16:30:44 -0200

    Octone e configuracao conexao

--- app/services/gitService.js
+++ app/services/gitService.js
@@ -1,7 +1,19 @@
+var github = require('octonode');
+
+
 var api = {};
 
+
 api.repository = (req, res) => {
-	
+
+     var client = github.client();
+     
+     var ghrepo = client.repo('CaioMichelSantos/apiKeepi');
+     
+    ghrepo.contents('', "master", ( err , status , body , headers ) => {
+        console.log(status); 
+
+    });
 };
 
 module.exports = api;

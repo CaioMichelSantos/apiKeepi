36ca4b0ec818f22bb2f2c0a70a5110a4257dc5de
Caio <caio.michel@gmail.com>
2017-10-21 16:22:37 -0200

    retirada de variavel nao utilizada

--- app/services/gitService.js
+++ app/services/gitService.js
@@ -4,11 +4,7 @@ var api = {};
 
 api.repository = (req, res) => {
     let git = req.body;
-    
     let client = github.client();
-
-    var gitList = {};
-
     let ghrepo = client.repo(git.nameAuthor + '/' + git.repository);
      
     ghrepo.contents('', git.branch, ( err , status , body , headers ) => {
